f26131bd06fc46ae6eb8cb3378beb727f72b2966
Eugene Wechsler <EugeneWechsler@gmail.com>
2014-01-09 20:23:33 +0700

    Changed download to StaticFileHandler

--- server/update-check-server.py
+++ server/update-check-server.py
@@ -8,24 +8,10 @@ import os
 import tornado.ioloop
 import tornado.web
 
-
-
 DEFAULT_PORT_NUMBER = 8888
 
 def binaryPath(package, variant):
     return os.path.join(packages.packagesPath,package,repr(variant['version']),variant['fileName'])
-
-def readBinary(path):
-    f = open(path)
-    data = f.read()
-    f.close()
-    return data
-
-
-def loadBinariesForPackage(package):
-    variants = packages.packages[package]
-    return dict([(variantId, readBinary(binaryPath(package,variant)))
-                 for variantId,variant in variants.items()])
 
 class CheckHandler(tornado.web.RequestHandler):
 
@@ -47,9 +33,8 @@ class CheckHandler(tornado.web.RequestHandler):
                 if variant:
                     lastVersion = variant['version']
                     if lastVersion > version:
-                        downloadLink = urlparse.urljoin(
-                            settings.downloadHostUrl,'download/%s?package_name=%s&variant_id=%s' %
-                                                     (variant['fileName'],packageName,variantId))
+                        downloadLink = urlparse.urljoin(settings.downloadHostUrl,'download/%s/%s/%s' %
+                                                     (packageName,lastVersion,variant['fileName']))
                         self.write('have update\n')
                         self.write(downloadLink)
                         return
@@ -57,35 +42,9 @@ class CheckHandler(tornado.web.RequestHandler):
         except tornado.web.MissingArgumentError as e:
             self.set_status(400, "Param is missing: %s" % e.arg_name)
 
-class DownloadHandler(tornado.web.RequestHandler):
-    binaries = None
-
-    def initialize(self):
-        super(DownloadHandler, self).initialize()
-        self.binaries = dict([(k,loadBinariesForPackage(k))
-                         for k in packages.packages.keys()])
-
-    def get(self):
-        try:
-            packageName = self.get_argument('package_name')
-            packageBinaries = self.binaries.get(packageName)
-            if packageBinaries:
-                binary = packageBinaries[self.get_argument('variant_id','')]
-            else:
-                binary = None
-
-            if binary:
-                self.set_status(200)
-                self.set_header('content-type','application/vnd.android.package-archive')
-                self.write(binary)
-            else:
-                self.set_status(404, "Package not found")
-        except tornado.web.MissingArgumentError as e:
-            self.set_status(400, "Param is missing: %s" % e.arg_name)
-
 application = tornado.web.Application([
     (r"/check", CheckHandler),
-    (r"/download/.*", DownloadHandler),
+    (r"/download/(.*)", web.StaticFileHandler, {"path": os.path.abspath(packages.packagesPath)}),
     ], debug=True)
 
 if __name__ == "__main__":
