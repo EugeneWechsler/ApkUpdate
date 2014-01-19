b5f9ae191006cbc0df779fef49719e3b77abbc5e
Eugene Wechsler <EugeneWechsler@gmail.com>
2014-01-19 17:17:47 +0700

    Added latest download and qr code generation

--- server/src/packages.py
+++ server/src/packages.py
@@ -3,7 +3,7 @@ packages = {
         'variant1': {
             'version': 22,
             'versionName': '2.02',
-            'fileName':'testVariant1.apk'
+            'fileName':'testVariant1'
         }
     }
 }
--- server/src/update-server.py
+++ server/src/update-server.py
@@ -1,40 +1,91 @@
 #!/usr/bin/python
 # coding=utf-8
 
-	# Copyright (c) 2012 Eugene Wechsler
-    #
-	# Licensed under the Apache License, Version 2.0 (the "License");
-	# you may not use this file except in compliance with the License.
-	# You may obtain a copy of the License at
-    #
-	# 	http://www.apache.org/licenses/LICENSE-2.0
-    #
-	# Unless required by applicable law or agreed to in writing, software
-	# distributed under the License is distributed on an "AS IS" BASIS,
-	# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	# See the License for the specific language governing permissions and
-	# limitations under the License.
+# Copyright (c) 2012 Eugene Wechsler
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# 	http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import StringIO
 
 import urlparse
 import os
 import sys
+import qrcode
 
 from tornado import web
 import tornado.ioloop
 from tornado.options import define, options
 import tornado.web
 
-
 import packages
 import settings
 
 define("port", default=8888, help="Port to listen on", type=int)
 
+
 def binaryPath(package, variant):
-    return os.path.join(settings.packagesPath,package,repr(variant['version']),variant['fileName'])
+    return os.path.join(settings.packagesPath, package, repr(variant['version']), variant['fileName'])
+
+
+def generateDownloadLink(request, packageName, variant):
+    hostUrl = request.protocol + '://' + request.host
+    downloadLink = urlparse.urljoin(hostUrl, 'download/%s/%s/%s' %
+                                             (packageName, variant['versionName'], variant['fileName'] + '.apk'))
+    return downloadLink
+
+class LatestHandler(tornado.web.RequestHandler):
+    EXT_APK = 'apk'
+    EXT_QR = 'qr'
+    def initialize(self):
+        super(LatestHandler, self).initialize()
+
+    def get(self, *args, **kwargs):
+        if args.__len__() != 2:
+            self.set_status(400)
+            return
+
+        if not args[1].__contains__('.'):
+            self.set_status(404)
+            return
+
+        fileName, extension = tuple(args[1].rsplit('.', 1))
+        fileName = fileName.lower()
+        extension = extension.lower()
+
+        if not extension in [self.EXT_APK, self.EXT_QR]:
+            self.set_status(404)
+            return
+
+        packageVariants = packages.packages.get(args[0])
+        downloadLink = None
+        if packageVariants:
+            matchingVariants = [v for k, v in packageVariants.iteritems() if v['fileName'].lower() == fileName]
+            if matchingVariants.__len__() > 0:
+                downloadLink = generateDownloadLink(self.request, args[0], matchingVariants[0])
+        if not downloadLink:
+            self.set_status(404)
+            return
+
+        if extension == self.EXT_APK:
+            self.redirect(downloadLink)
+        elif extension == self.EXT_QR:
+            img = qrcode.make(downloadLink)
+            self.set_header('content-type','image/png')
+            output = StringIO.StringIO()
+            img.save(output, 'PNG')
+            self.write(output.getvalue())
+
 
 class CheckHandler(tornado.web.RequestHandler):
-
     def initialize(self):
         super(CheckHandler, self).initialize()
 
@@ -43,7 +94,6 @@ class CheckHandler(tornado.web.RequestHandler):
             print('Received check request:\n %s' % self.request.body)
             args = dict(urlparse.parse_qsl(self.request.body))
             if not args.has_key('pkgname'):
-
                 return
             if not args.has_key('variant_id'):
                 self.set_status(400, "Param is missing: variant_id")
@@ -61,17 +111,15 @@ class CheckHandler(tornado.web.RequestHandler):
             if not variantId:
                 variantId = ''
             self.set_status(200)
-            self.set_header('content-type','text/plain')
+            self.set_header('content-type', 'text/plain')
 
             packageVariants = packages.packages.get(packageName)
             if packageVariants:
-                variant=packageVariants.get(variantId)
+                variant = packageVariants.get(variantId)
                 if variant:
                     lastVersion = variant['version']
                     if lastVersion > version:
-                        hostUrl = self.request.protocol + '://' + self.request.host
-                        downloadLink = urlparse.urljoin(hostUrl,'download/%s/%s/%s' %
-                                                     (packageName,variant['versionName'],variant['fileName']))
+                        downloadLink = generateDownloadLink(self.request, packageName, variant)
                         self.write('have update\n')
                         self.write(downloadLink)
                         return
@@ -84,13 +132,16 @@ def main():
     tornado.options.parse_command_line()
 
     application = tornado.web.Application([
-        (r"/check", CheckHandler),
-        (r"/download/(.*)", web.StaticFileHandler, {"path": os.path.abspath(settings.packagesPath)}),
-        ], debug=True)
+                                              (r"/check", CheckHandler),
+                                              (r"/download/(.*)", web.StaticFileHandler,
+                                               {"path": os.path.abspath(settings.packagesPath)}),
+                                              (r"/(.*)/(.*)", LatestHandler),
+                                          ], debug=True)
     application.listen(tornado.options.options.port)
-    print 'Started server on port ' , options.port
+    print 'Started server on port ', options.port
     sys.stdout.flush()
     tornado.ioloop.IOLoop.instance().start()
+
 
 if __name__ == "__main__":
     main()
