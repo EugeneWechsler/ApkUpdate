22089ff5b8cdc19ca252ff4c802245006b156a77
Eugene Wechsler <eugenewechsler@gmail.com>
2017-11-19 18:28:41 -0500

    Revern unneded things form previous commit

--- server/deploy/push.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-export MYDIR=$(dirname "${BASH_SOURCE[0]}")
-eval $(docker-machine env $DOCKER_MACHINE)
-docker-compose -f $MYDIR/../docker-compose.yml -f $MYDIR/../docker-compose.push.yml build
-docker-compose -f $MYDIR/../docker-compose.yml -f $MYDIR/../docker-compose.push.yml push
